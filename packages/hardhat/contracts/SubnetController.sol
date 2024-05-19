2946d22ad30265a5807c6144861256d3f247412b
Parth Gupta <parth.eng1210@gmail.com>
2024-05-19 17:13:46 +0530

    feat(contracts): added ipc contracts

---
+++ packages/hardhat/contracts/SubnetController.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import {IGateway} from "./ipc/interfaces/IGateway.sol";
+import {SubnetID} from "./ipc/structs/Subnet.sol";
+import {FvmAddress} from "./ipc/structs/FvmAddress.sol";
+
+contract SubnetController {
+    address public immutable gateway;
+    constructor (address _gateway) {
+        gateway = _gateway;
+    }
+
+    function fund(SubnetID calldata subnetId, FvmAddress calldata to, uint256 amount) public {
+        IGateway _gateway  = IGateway(gateway);
+        _gateway.fundWithToken(subnetId, to, amount);
+    }
+
+    function release(FvmAddress calldata to) public payable {
+        IGateway _gateway  = IGateway(gateway);
+        _gateway.release(to);
+    }
+}
